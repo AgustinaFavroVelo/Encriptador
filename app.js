c80c673d8443f9fed7ef4250b855007533d589e1
AgustinaFavroVelo <131202997+AgustinaFavroVelo@users.noreply.github.com>
2024-01-28 18:35:20 -1100

    mejorando indentaciones en codigo

--- app.js
+++ app.js
@@ -31,21 +31,21 @@ function desencriptar() {
     let meme = document.getElementById("meme");
 
     let textoCifrado = texto
-    .replace(/enter/gi, "e")
-    .replace(/imes/gi, "i")
-    .replace(/ai/gi, "a")
-    .replace(/ober/gi, "o")
-    .replace(/ufat/gi, "u");
+        .replace(/enter/gi, "e")
+        .replace(/imes/gi, "i")
+        .replace(/ai/gi, "a")
+        .replace(/ober/gi, "o")
+        .replace(/ufat/gi, "u");
   
     if (texto.length != 0) {
-      document.getElementById("text").value = textoCifrado;
-      tituloMensaje.textContent = "Texto desencriptado con éxito";
-      parrafo.textContent = "";
-      meme.src = "./img/Dogecoin-2-1200x640.jpg";
+        document.getElementById("text").value = textoCifrado;
+        tituloMensaje.textContent = "Texto desencriptado con éxito";
+        parrafo.textContent = "";
+        meme.src = "./img/Dogecoin-2-1200x640.jpg";
     } else {
-      meme.src = "./img/angry-rat.jpg";
-      tituloMensaje.textContent = "Ningún mensaje fue encontrado";
-      parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
-      swal("Ooops!", "Debes ingresar un texto", "warning");
+        meme.src = "./img/angry-rat.jpg";
+        tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+        swal("Ooops!", "Debes ingresar un texto", "warning");
     }
 }
